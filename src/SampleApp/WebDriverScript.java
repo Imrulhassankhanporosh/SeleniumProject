6f888eb2f2056bf88b164e77489e00b0f52712f8
Imrul Hassan Khan <imrul.hassan@surecash.net>
2022-03-13 00:12:35 +0600

    3rd commit

---
+++ src/SampleApp/WebDriverScript.java
@@ -0,0 +1,30 @@
+package SampleApp;
+
+import org.openqa.selenium.WebDriver;
+
+//contains the WebDriver class needed to instantiate a new browser loaded with a specific driver
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//contains the FirefoxDriver class needed to instantiate a Firefox-specific driver onto the browser instantiated by the WebDriver class
+
+public class WebDriverScript {
+    public static void main(String[] args) {
+
+        // declaration and instantiation of objects/variables
+
+       System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+
+        //comment the above 2 lines and uncomment below 2 lines to use Firefox
+
+        //System.setProperty("webdriver.gecko.driver","D:\\geckodriver.exe");
+        //WebDriver driver = new FirefoxDriver();
+
+        //String baseUrl = "http://demo.guru99.com/test/newtours/";
+        //String baseUrl = "http://www.google.com";
+        driver.get("http://www.google.com");
+        //driver.quit();
+    }
+}
